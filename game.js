59afb84074ea8668769d90518b36cadbf56da70b
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-14 08:26:48 +0600

    added an DOM Element by js

--- game.js
+++ game.js
@@ -6,13 +6,16 @@ const gamearea = document.querySelector('.gamearea');
 var keys = {
     LeftArrow:false,Uparrow:false,Rightarrow:false,Downarrow:false
 }
+var player = {};
 startscreen.addEventListener('click',start);
 //keypress addEventListener();
 document.addEventListener('keydown',pressOn);
 document.addEventListener('keyup',pressOff);
 function playgame(){
     console.log('palyed');
+    if(player.start){
     window.requestAnimationFrame(playgame);
+    }
 }
 function pressOn(e){
     e.preventDefault();//Removed default value now set new value
@@ -26,5 +29,14 @@ function pressOff(e){
 }
 function start(){
     console.log('animation started');
+    player.start = true;
     window.requestAnimationFrame(playgame);
+    //now add and remove the hide class from an element;
+    startscreen.classList.add('hide');
+    gamearea.classList.remove('hide');
+    //now creating a element for the game car;
+    let car = document.createElement('div');
+    car.innerText = 'car';
+    gamearea.appendChild(car);
+    
 }
